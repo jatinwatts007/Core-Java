3a65bf6779f93c7ebdacc1bbfc73fedbf953d3ae
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-04-19 20:12:10 +0530

    Create OOPS demo.java

---
+++ OOPS demo.java
@@ -0,0 +1,13 @@
+package casestudy;
+class demo{
+	void printMessage() {
+		System.out.println("you are welcome");
+	}
+};
+public class oopsdemo {
+	public static void main(String[] args) {
+		demo d1= new demo();
+		d1.printMessage();
+	}
+
+}
