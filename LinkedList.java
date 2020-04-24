ad0be1ac3e7b6f2475950680cfcd83213fe1646f
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-04-24 21:36:54 +0530

    Create LinkedList.java

---
+++ LinkedList.java
@@ -0,0 +1,12 @@
+package CoreJava;
+import java.util.*;
+public class linkList {
+	public static void main(String[]args) {
+		String str = "Welcome";
+		LinkedList<String> ll = new LinkedList<String>();
+		ll.add("Jatin");
+		ll.add("Joker");
+		ll.add("James");
+		System.out.println(str+ll);
+	}
+}
