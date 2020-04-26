12d563d29169981a549e89e7311b3667727669fe
Jatin Watts <51354333+jatinwatts007@users.noreply.github.com>
2020-04-26 20:38:18 +0530

    Create ThreadPoolExample.java

---
+++ ThreadPoolExample.java
@@ -0,0 +1,35 @@
+package CoreJava;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+class WorkerThread implements Runnable{
+	private String message;
+	public WorkerThread(String s) {
+		this.message = s;
+	}
+	public void run() {
+		System.out.println(Thread.currentThread().getName()+"(start) message = "+message);
+		processmessage();
+		System.out.println(Thread.currentThread().getName()+"(end)");
+	}
+	private void processmessage() {
+		try {
+			Thread.sleep(2000);
+		}catch(InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+}
+public class ThreadPoolExample {
+	public static void main(String[]args) {
+		ExecutorService executor = Executors.newFixedThreadPool(5);
+		for(int i=0;i<10;i++) {
+			Runnable worker = new WorkerThread("" + i);
+			executor.execute(worker);
+		}
+		executor.isShutdown();
+		while(!executor.isTerminated()) {
+			
+		}
+		System.out.println("finished all threads");
+	}
+}
